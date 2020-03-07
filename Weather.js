db981df2ab1c84f97aea806f39b04cabdab10e83
wwlee94 <wwlee94@naver.com>
2020-03-08 03:57:45 +0900

    Fix: 아이콘, 설명 수정

--- Weather.js
+++ Weather.js
@@ -5,15 +5,74 @@ import { MaterialCommunityIcons } from '@expo/vector-icons';
 import { LinearGradient } from 'expo-linear-gradient';
 
 const weatherOptions = {
+    Rain: {
+        iconName: 'weather-rainy',
+        gradient: ['#00C6EB', '#005BEA'],
+        title: "Rainy",
+        subtitle: "It's raining 🌧"
+    },
+    Clear: {
+        iconName: 'white-balance-sunny',
+        gradient: ['#FEF253', '#FF7300'],
+        title: 'Sunny',
+        subtitle: "Let's go out and play ☀️!!"
+    },
+    Thunderstorm: {
+        iconName: 'weather-lightning',
+        gradient: ['#373B44', '#4286F4'],
+        title: 'Thunderstorm',
+        subtitle: ''
+    },
+    Clouds: {
+        iconName: 'apple-icloud',
+        gradient: ['#D7D2CC', '#304352'],
+        title: 'Cloudy',
+        subtitle: ''
+    },
+    Snow: {
+        iconName: 'snowflake',
+        gradient: ['#7DE2FC', '#B9B6E5'],
+        title: 'Snowy',
+        subtitle: 'Do you want to build a snowman? ☃️'
+    },
+    Drizzle: {
+        iconName: 'weather-hail',
+        gradient: ['#89F7FE', '#66A6FF'],
+        title: 'Drizzle',
+        subtitle: "It's like rain 🌦"
+    },
+    Atmosphere: {
+        iconName: 'weather-hail',
+        gradient: ['#89F7FE', '#66A6FF'],
+        title: '',
+        subtitle: ''
+    },
+    Mist: {
+        iconName: 'weather-fog',
+        gradient: ['#eef2f3', '#8e9eab'],
+        title: 'Fogy',
+        subtitle: ''
+    },
     Haze: {
-        iconName: 'weather-hail'
+        iconName: 'weather-fog',
+        gradient: ['#D7D2CC', '#304352'],
+        title: 'Haze',
+        subtitle: "It's like you have no glasses on."
+    },
+    Dust: {
+        iconName: 'weather-fog',
+        gradient: ['#D39D38', '#D7D2CC'],
+        title: 'Dusty',
+        subtitle: ''
     }
 }
 
 export default function Weather({ temp, condition }) {
+    console.log('Condition: ' + condition);
+    condition = 'Mist'
     return (
         <LinearGradient
-            colors={['#4c669f', '#3b5998', '#192f6a']}
+            colors={weatherOptions[condition].gradient}
             style={styles.container}
         >
             <StatusBar barStyle='light-content' />
@@ -25,7 +84,9 @@ export default function Weather({ temp, condition }) {
                 />
                 <Text style={styles.temp}>{temp}</Text>
             </View>
-            <View style={styles.halfContainer}>
+            <View style={{ ...styles.halfContainer, ...styles.textContainer }}>
+                <Text style={styles.title}>{weatherOptions[condition].title}</Text>
+                <Text style={styles.subtitle}>{weatherOptions[condition].subtitle}</Text>
             </View>
         </LinearGradient>
     );
@@ -54,12 +115,29 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     temp: {
-        fontSize: 32,
-        color: 'white'
+        fontSize: 48,
+        color: 'white',
+        marginTop: 10
     },
     halfContainer: {
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
+    },
+    title: {
+        color: 'white',
+        fontSize: 38,
+        fontWeight: '300',
+        marginBottom: 20
+    },
+    subtitle: {
+        fontSize: 24,
+        fontWeight: '600',
+        color: 'white',
+        marginBottom: 50
+    },
+    textContainer: {
+        paddingHorizontal: 25,
+        alignItems: 'flex-start'
     }
 })
